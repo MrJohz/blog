02b8162a33bbe837f167405441f59e09c40634df
Jonathan Frere <jonathan.frere@gmail.com>
2024-09-25 22:34:41 +0200

    Update text sharing mechanism to actually work

--- themes/johz/assets/js/utils.js
+++ themes/johz/assets/js/utils.js
@@ -7,32 +7,23 @@ window.toggletheme = () => {
 };
 
 function handleShare() {
-  document.body.append(`can share? ${navigator.canShare}`);
+  // N.B. this code is difficult to debug as it requires the right browser,
+  // the right device _and_ must be served via a secure context.
   if (!navigator.canShare) return;
-  document.body.append(`can share 2? ${navigator.canShare({ text: "???" })}`);
   if (!navigator.canShare({ text: "???" })) return;
-  document.body.append(`has element? ${document.getElementById("share")}`);
   const shareButton = document.getElementById("share");
   if (!shareButton) return;
 
-  document.body.append("added class");
   document.body.classList.add("share-enabled");
 
-  document.body.append("added click listener");
   shareButton.addEventListener("click", () => {
     console.log("clicked!");
     console.log(
       navigator.share({
-        text: "???",
+        text: document.title,
+        url: String(document.location),
+        title: document.title,
       })
     );
   });
-}
-
-try {
-  handleShare();
-} catch (e) {
-  const pre = document.createElement("pre");
-  pre.innerText = e.stack;
-  document.body.append(pre);
 }
