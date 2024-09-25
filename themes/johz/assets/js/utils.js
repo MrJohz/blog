def09c96a8bd0ef645689dabab574e7a6493b6c1
Jonathan Frere <jonathan.frere@gmail.com>
2024-09-25 22:39:34 +0200

    Next sharing test

--- themes/johz/assets/js/utils.js
+++ themes/johz/assets/js/utils.js
@@ -9,21 +9,32 @@ window.toggletheme = () => {
 function handleShare() {
   // N.B. this code is difficult to debug as it requires the right browser,
   // the right device _and_ must be served via a secure context.
+  const share = {
+    text: document.title,
+    url: String(document.location),
+    title: document.title,
+  };
+
+  document.body.append(`to share: ${JSON.stringify(share)}`);
+  document.body.append(`can share? ${navigator.canShare}`);
   if (!navigator.canShare) return;
-  if (!navigator.canShare({ text: "???" })) return;
+  document.body.append(`can share 2? ${navigator.canShare(share)}`);
+  if (!navigator.canShare(share)) return;
+  document.body.append(`has element? ${document.getElementById("share")}`);
   const shareButton = document.getElementById("share");
   if (!shareButton) return;
 
+  document.body.append("added class");
   document.body.classList.add("share-enabled");
 
-  shareButton.addEventListener("click", () => {
-    console.log("clicked!");
-    console.log(
-      navigator.share({
-        text: document.title,
-        url: String(document.location),
-        title: document.title,
-      })
-    );
-  });
+  document.body.append("added click listener");
+  shareButton.addEventListener("click", () => navigator.share(share));
+}
+
+try {
+  handleShare();
+} catch (e) {
+  const pre = document.createElement("pre");
+  pre.innerText = e.stack;
+  document.body.append(pre);
 }
